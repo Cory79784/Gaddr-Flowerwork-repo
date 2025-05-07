410acd55d0916994cf8be266672f262b7e0520f7
Houye <salahdhy@gmail.com>
2025-05-07 21:54:23 +0200

    feat: add outsource-talent page route

---
+++ frontend/app/outsource-talent/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import OutsourceTalent from "../components/OutsourceTalent"
+
+export default function OutsourceTalentMobile() {
+    return (
+      <div>
+        <OutsourceTalent />
+      </div>
+    );
+  }
